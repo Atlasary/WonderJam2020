0185a4d0d4b76f80075a07ed8404ef6693e8613f
TonyColas <61602857+TonyColas@users.noreply.github.com>
2020-02-28 18:40:34 -0500

    script utilitaire (deplacement ZQSD)

---
+++ Exflitration/Assets/Scripts/Autres/movable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movable : MonoBehaviour
+{
+public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z)) {
+            transform.Translate(Vector3.up*Time.deltaTime*speed);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            transform.Translate(Vector3.down*Time.deltaTime*speed);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Translate(Vector3.right*Time.deltaTime*speed);
+        }
+        if (Input.GetKey(KeyCode.Q)) {
+            transform.Translate(Vector3.left*Time.deltaTime*speed);
+        }
+    }
+}
