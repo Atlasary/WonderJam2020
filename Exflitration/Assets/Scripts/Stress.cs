05de5b3b0181ae0f101418311a80ca37bdde7b73
Baptiste <batzr84@gmail.com>
2020-02-29 08:27:17 -0600

    StressBar en prefab !

--- Exflitration/Assets/Scripts/Stress.cs
+++ Exflitration/Assets/Scripts/Stress.cs
@@ -6,16 +6,37 @@ using UnityEngine;
 
 public class Stress : MonoBehaviour
 {
+
+    // Jauge de Stress du survivant
+    public float Stress = 0f;
+
+    // Insertion des GameObject
+    public GameObject Killer;
+    public GameObject StressBar;
+
+    // Position du Survivant et du Tueur
+    private Vector3 PositionKiller;
+    private Vector3 PositionSurvivor;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Killer = null)
+            Killer = GameObject.Find("Mad");
+        PositionKiller = Killer.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Avec le temps le taux de stress du survivant diminue
+        if (Stress > 0)
+        {
+
+        }
+        // Avec l'éloignement par rapport au tueur, le taux de stress diminue
+        if ()
     }
 
     void EnterVision()
