e8b504c5256101d2671dd8104df4ac3528f9d2e1
TonyColas <61602857+TonyColas@users.noreply.github.com>
2020-02-29 10:28:24 -0500

    add focus broadcast

--- Exflitration/Assets/Scripts/MadVision.cs
+++ Exflitration/Assets/Scripts/MadVision.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MadVision : MonoBehaviour
 {
+
+    private GameObject focus = null;
+    //private List<GameObject> enVision = new List<GameObject>;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,18 +21,18 @@ public class MadVision : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D obj) 
+    private void OnTriggerEnter2D(Collider2D col) 
     {
-        if (obj.tag == "People") {
-            if (peopleInVision(obj)) {
-                seeSomeone(obj);
+        if (col.tag == "People") {
+            if (peopleInVision(col)) {
+                seeSomeone(col.gameObject);
             }
         }
     }
 
     bool peopleInVision(Collider2D aim) 
     {
-         int layerMask = ~(1 << 2); // all except 2
+        int layerMask = ~(1 << 2); // all except 2
         RaycastHit2D hit = Physics2D.Linecast(transform.position, aim.transform.position, layerMask , -Mathf.Infinity, Mathf.Infinity);
         // If it hits something...
         if (hit.collider != null) 
@@ -37,15 +41,38 @@ public class MadVision : MonoBehaviour
             {
                 return true;
             }
+            if (hit.collider.tag == "Hiding")
+            {
+                layerMask = 1 << 10; // hiding
+                //RaycastHit2D hit = Physics2D.Linecast(transform.position, aim.transform.position, layerMask , -Mathf.Infinity, Mathf.Infinity);
+            }
         }
 
         return false; 
     }
 
-    private void seeSomeone(Collider2D people)
+    private void seeSomeone(GameObject people)
     {
+
         Debug.Log("See someone");
         people.BroadcastMessage("EnterVision");
+        estEnFocus(people);
+        /*
+        if (!estEnVision(people)) 
+        {
+            people.BroadcastMessage("EnterVision");
+        }*/
+    }
+
+    private void estEnVision() 
+    {
+        //return enVision.Contains(people);
+    }
+
+    private void estEnFocus(GameObject target)
+    {
+        focus = target;
+        gameObject.BroadcastMessage("updateFocus", target);
     }
 
     private void OnTriggerExit2D(Collider2D obj) 
