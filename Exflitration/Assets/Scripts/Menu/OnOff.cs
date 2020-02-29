be5421e6663c1e25c23622cb39270e7a62434ab1
Manon <manon.piquemal@gmail.com>
2020-02-29 16:46:48 -0600

    modif On Off audio

--- Exflitration/Assets/Scripts/Menu/OnOff.cs
+++ Exflitration/Assets/Scripts/Menu/OnOff.cs
@@ -8,12 +8,14 @@ using UnityEngine.Events;
 public class OnOff : MonoBehaviour
 {
     public Button Button_son;
+    AudioSource audioData;
     public TextMeshProUGUI Button_sontext;
     // Start is called before the first frame update
     void Start()
     {
         Button_sontext = Button_son.GetComponentInChildren<TextMeshProUGUI>();
         Button_son.onClick.AddListener(changeTxt);
+        audioData = GetComponent<AudioSource>();
     }
     
     public void changeTxt()
@@ -22,11 +24,12 @@ public class OnOff : MonoBehaviour
         if(btn == "On")
         {
             Button_sontext.text = "Off";
-
+            audioData.mute = !audioData.mute;
         }
         else
         {
             Button_sontext.text = "On";
+            audioData.mute = !audioData.mute;
         }
     }
 
