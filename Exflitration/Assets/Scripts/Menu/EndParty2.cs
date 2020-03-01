438c1b2555a0762fd935f5946e24a66295873330
Manon <manon.piquemal@gmail.com>
2020-02-29 23:05:30 -0600

    win lose entre les niveaux

---
+++ Exflitration/Assets/Scripts/Menu/EndParty2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndParty2 : MonoBehaviour
+{
+	bool end = true;
+	public Button Win;
+	Button btn;
+
+	void Start()
+	{
+		btn = Win.GetComponent<Button>();
+    	btn.onClick.AddListener(res);
+	}
+
+    void res()
+    {
+        if(end == false)
+        {
+        	SceneManager.LoadScene("Lose");
+        }
+        else
+        {
+        	SceneManager.LoadScene("Win3");
+        }
+    }
+}
---
+++ Exflitration/Assets/Scripts/Menu/Replayed.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Replayed : MonoBehaviour
+{
+    public Button Replay;
+    Button btn;
+
+    void Start()
+    {
+        btn = Replay.GetComponent<Button>();
+    	btn.onClick.AddListener(replay);
+    }
+
+    void replay()
+    {
+        SceneManager.LoadScene("SelectLevels");
+    }
+}
